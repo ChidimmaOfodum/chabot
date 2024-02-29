460aa7ed18bd3ecc8135e217220c575df5e5e690
Chidimma <chidimmamofodum@gmail.com>
2024-02-29 02:26:36 -0500

    error fix

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 set -eo pipefail
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
 nvm install v20.11.0
 nvm alias default 20.11.0
 
